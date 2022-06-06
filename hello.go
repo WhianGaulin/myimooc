249ddd10688dab749add6a1ea50f89cd7c80bd76
zheyan <zheyan.wang@outlook.com>
2022-06-06 14:23:31 +0800

    toy

---
+++ hello.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/WhianGaulin/myimooc/morestrings"
+	"github.com/google/go-cmp/cmp"
+)
+
+func main() {
+	fmt.Println("Hello, world.")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+	fuckFromWZY()
+}
+
+func fuckFromWZY() {
+	fmt.Println("fuck you hey yo!")
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+}
