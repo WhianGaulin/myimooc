2062abee14b0266c3d8655bfdaa180cdd75a27cc
zheyan <zheyan.wang@outlook.com>
2022-06-06 18:08:22 +0800

    share fuck

---
+++ fuck/fuckyou.go
@@ -0,0 +1,7 @@
+package fuck
+
+func Fuckwho(s string) string {
+	s1 := "fuck"
+	r := s1 + ":" + s
+	return string(r)
+}
--- hello.go
+++ hello.go
@@ -1,16 +1,19 @@
-package hello
+package main
 
 import (
 	"fmt"
+	"github.com/WhianGaulin/myimooc/fuck"
+	"github.com/WhianGaulin/myimooc/morestrings"
 	"github.com/google/go-cmp/cmp"
 )
 
-//func main() {
-//	fmt.Println("Hello, world.")
-//	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
-//	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
-//	fuckFromWZY()
-//}
+func main() {
+	fmt.Println("Hello, world.")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+	fuckFromWZY()
+	fmt.Println(fuck.Fuckwho("you"))
+}
 
 func fuckFromWZY() {
 	fmt.Println("fuck you hey yo!")
